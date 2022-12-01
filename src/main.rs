76e6145840d37bdef81b58d13f0f1f6cf32cce33
Henrik Eckhardt <henrik-eckhardt@t-online.de>
2022-12-01 22:18:12 +0100

    feat: go home CLion, you're drunk

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use rust_embed::RustEmbed;
+
+mod day1;
+
+#[derive(RustEmbed)]
+#[folder = "input/"]
+struct Input;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Advent of Code 2022\n");
+
+    let day1 = day1::solve();
+    println!("Day 1\n\tPart One - {}\n\tPart Two - {}\n", day1.0, day1.1);
+}
+
+fn input(file_path: &str) -> String {
+    let input = Input::get(file_path).unwrap();
+    String::from_utf8(Vec::from(input.data)).unwrap()
 }
