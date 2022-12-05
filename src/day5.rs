be92cf7cdb866af32272d6d7a7afd6c76ed8e10e
Henrik Eckhardt <henrik-eckhardt@t-online.de>
2022-12-05 19:44:33 +0100

    feat: day 5 part 2

--- src/day5.rs
+++ src/day5.rs
@@ -19,25 +19,35 @@ pub fn solve() {
         }
     }
 
+    let mut part_one = stacks.clone();
+    let mut part_two = stacks;
     for line in parts[1].lines() {
         let vec: Vec<usize> = line
             .split_whitespace()
             .filter_map(|part| part.parse().ok())
             .collect();
-        let from = stacks.get_mut(vec[1] - 1).unwrap();
-        let mut new = Vec::new();
+
+        let from = vec[1] - 1;
+        let from_one = part_one.get_mut(from).unwrap();
+        let from_two = part_two.get_mut(from).unwrap();
+
+        let mut to_one = Vec::new();
+        let mut to_two = Vec::new();
         for _ in 0..vec[0] {
-            if let Some(c) = from.pop() {
-                new.push(c);
-            }
+            to_one.push(from_one.pop().unwrap());
+            to_two.push(from_two.pop().unwrap());
         }
-        stacks
-            .get_mut(vec[2] - 1)
-            .unwrap()
-            .extend_from_slice(new.as_slice());
+
+        let to = vec[2] - 1;
+        part_one.get_mut(to).unwrap().extend(to_one);
+        part_two.get_mut(to).unwrap().extend(to_two.iter().rev());
     }
 
-    let part_one: String = stacks.iter().map(|stack| stack.last().unwrap()).collect();
+    let part_one: String = part_one.iter().map(|stack| stack.last().unwrap()).collect();
+    let part_two: String = part_two.iter().map(|stack| stack.last().unwrap()).collect();
 
-    println!("Day 5\n\tPart One - {}\n\tPart Two - {}\n", part_one, 0);
+    println!(
+        "Day 5\n\tPart One - {}\n\tPart Two - {}\n",
+        part_one, part_two
+    );
 }
